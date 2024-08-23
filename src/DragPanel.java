f4c208b8a0d9541b0ee0a36280995840d8a3a64f
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-08-23 13:23:51 +0300

    add an imageIcon to drayPanel

--- src/DragPanel.java
+++ src/DragPanel.java
@@ -1,4 +1,9 @@
 import javax.swing.*;
 
 public class DragPanel extends JPanel {
+    ImageIcon image = new ImageIcon("smile.jpeg");
+
+    DragPanel(){
+
+    }
 }
--- src/MyFrame.java
+++ src/MyFrame.java
@@ -5,6 +5,7 @@ public class MyFrame extends JFrame {
     DragPanel dragPanel = new DragPanel();
 
     MyFrame(){
+        this.add(dragPanel);
         this.setTitle("Drag & Drop demo");
         this.setSize(600,600);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
