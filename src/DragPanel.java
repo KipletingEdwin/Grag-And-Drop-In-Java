614f5f3bef526350b5e6e3632783b0bb65d81b9d
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-08-23 14:46:40 +0300

    instanciate clickListener

--- src/DragPanel.java
+++ src/DragPanel.java
@@ -1,11 +1,18 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseMotionAdapter;
 
 public class DragPanel extends JPanel {
     ImageIcon image = new ImageIcon("smile.jpeg");
+    final  int WIDTH = image.getIconWidth();
+    final  int HEIGHT = image.getIconHeight();
+    Point imageCorner;
+    Point prevPt;
 
     DragPanel(){
+        imageCorner = new Point(0,0);
+        ClickListener clickListener = new ClickListener();
 
     }
 
