5468dbd1e0f2e2559e9350f26379605698f39913
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-08-23 11:59:14 +0300

    setDefaultCloseOperation to ExitOnClose

--- src/MyFrame.java
+++ src/MyFrame.java
@@ -5,6 +5,8 @@ public class MyFrame extends JFrame {
     MyFrame(){
         this.setTitle("Drag & Drop demo");
         this.setSize(600,600);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
 
 
 
