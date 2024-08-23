e7161d8359f7637c510578e4d9ee5c0f9378acc6
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-08-23 23:51:39 +0300

    add mousePressed method

--- src/DragPanel.java
+++ src/DragPanel.java
@@ -1,6 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionAdapter;
 
 public class DragPanel extends JPanel {
@@ -21,11 +22,15 @@ public class DragPanel extends JPanel {
 
     public  void paintComponent(Graphics g){
         super.paintComponent(g);
-        image.paintIcon(this,g,imageCorner.getX(),imageCorner.getY());
+        image.paintIcon(this,g,(int)imageCorner.getX(),(int) imageCorner.getY());
 
     }
 
     private class ClickListener extends MouseAdapter {
+        public  void  mousePressed(MouseEvent e){
+            prevPt = e.getPoint();
+
+        }
 
     }
 
